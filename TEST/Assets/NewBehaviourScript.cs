a496c84504b72c6bdb57b14458be72788c19d9f6
NivGarbelly <60030777+NivGarbelly@users.noreply.github.com>
2020-12-21 17:38:40 +0200

    .

---
+++ TEST/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private GameObject thisOBJ;
+    private GameObject otherOBJ;
+
+    private void Update()
+    {
+        transform.localScale = transform.localScale * 1.01f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        otherOBJ = other.gameObject;
+        thisOBJ = this.gameObject;
+        Invoke("Stick",0.25f);
+    }
+
+    private void Stick()
+    {
+        otherOBJ.transform.SetParent(thisOBJ.transform,true);
+    }
+}
