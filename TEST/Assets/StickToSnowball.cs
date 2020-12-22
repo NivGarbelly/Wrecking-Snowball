0985cb477e898521d989e86487efc0c4e806b800
kozana123 <57573065+kozana123@users.noreply.github.com>
2020-12-22 20:24:53 +0200

    s

--- TEST/Assets/StickToSnowball.cs
+++ TEST/Assets/StickToSnowball.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class StickToSnowball : MonoBehaviour
 {
+
+    private FixedJoint fixJoint;
+    private GameObject snowBall;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        fixJoint = GetComponent<FixedJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            UnStick();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        snowBall = other.gameObject;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Stick", 0.25f);
+
+        }
+
+    }
+
+    private void Stick()
+    {
+        fixJoint.connectedBody = snowBall.GetComponent<Rigidbody>();
+    }
+
+    public void UnStick()
+    {
+        Debug.Log("work");
+        fixJoint.breakForce = 1f;
+        fixJoint.breakTorque = 1f;
+        fixJoint.enableCollision = true;
+        var col = GetComponent<Collider>();
+        col.isTrigger = false;
     }
 }
