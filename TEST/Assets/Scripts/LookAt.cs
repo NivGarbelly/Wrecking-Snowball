bf5b49c269b32fd21410a873d45590db012d251d
NivGarbelly <60030777+NivGarbelly@users.noreply.github.com>
2020-12-28 14:03:53 +0200

    Added Debug Menu to Canvas

--- TEST/Assets/Scripts/LookAt.cs
+++ TEST/Assets/Scripts/LookAt.cs
@@ -1,19 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookAt : MonoBehaviour
 {
     public Transform player;
-    public float smooth = 0.125f;
+    public float smooth = 1f;
     public Vector3 offset;
+    public Text xText;
+    public Text yText;
+    public Text zText;
+    public Slider xSlider;
+    public Slider ySlider;
+    public Slider zSlider;
     private void LateUpdate()
     {
         if (player != null)
-        {
-         Vector3 desiredPos = player.position + offset;
+        { Vector3 desiredPos = player.position + offset;
          Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smooth);
          transform.position = smoothedPos;
         }
+    }
+
+    private void Update()
+    {
+        offset.x = xSlider.value;
+        offset.y = ySlider.value;
+        offset.z = zSlider.value;
+        xText.text = offset.x.ToString();
+        yText.text = offset.y.ToString();
+        zText.text = offset.z.ToString();
+    }
+
+    public void ResetButton()
+    {
+        xSlider.value = 0;
+        ySlider.value = 10;
+        zSlider.value = -20;
     }
 }
--- TEST/Assets/Scripts/SnowBall.cs
+++ TEST/Assets/Scripts/SnowBall.cs
@@ -1,41 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SnowBall : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    public float steerForce =2;
+    public float steerForce =10;
     public float speed=1;
-    public float speedChangeForce = 0.01f;
+    public float speedChangeForce = 0.075f;
     public FloatingJoystick variableJoystick;
     public GameObject[] collect;
-
+    public Text steerForceText;
+    public Text speedChangeText;
+    public Slider steerForceSlider; 
+    public Slider speedChangeSlider;
+    public bool isPaused = false;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         speed=1;
     }
-
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(Vector3.forward * speed*2);
-        speed = speed + speedChangeForce;
-        transform.localScale = new Vector3(speed, speed, speed)/10;
-        _rigidbody.mass = speed/3;
-        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
-        _rigidbody.AddForce(direction * steerForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        if (isPaused==false)
+        {
+         _rigidbody.AddForce(Vector3.forward * speed*2);
+         speed = speed + speedChangeForce;
+         transform.localScale = new Vector3(speed, speed, speed)/10;
+         _rigidbody.mass = speed/3;
+         Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+         _rigidbody.AddForce(direction * steerForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
     }
-
+    public void Update()
+    {
+        
+            speedChangeText.text = speedChangeForce.ToString();
+            speedChangeForce = speedChangeSlider.value;
+            steerForceText.text = steerForce.ToString();
+            steerForce = steerForceSlider.value;   
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             speed = speed * 0.9f;
         }
-
-        
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Finish"))
@@ -43,7 +57,6 @@ public class SnowBall : MonoBehaviour
             DestroyFun();
         }
     }
-
     private void DestroyFun()
     {
         collect = GameObject.FindGameObjectsWithTag("Collect");
@@ -52,5 +65,26 @@ public class SnowBall : MonoBehaviour
             Obj.gameObject.GetComponent<StickObjects_INT>()?.UnStick();
         }
         Destroy(this.gameObject);
+    }
+    public void SpeedChangeReset()
+    {
+        speedChangeSlider.value = 0.075f;
+    }
+    public void SteerForceReset()
+    {
+        steerForceSlider.value =10;
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void IsPausedFalse()
+    {
+        isPaused = false;
+    }
+    public void IsPausedTrue()
+    {
+        isPaused = true;
     }
 }
